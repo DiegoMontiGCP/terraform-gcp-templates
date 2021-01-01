1b97bff7982958ce4b9b39d3fd916d6df731e93c
Adam Bekila <adam.b@datatonic.com>
2021-01-01 23:47:55 +0000

    Project initial setup

---
+++ variables.tf
@@ -0,0 +1,55 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "composer_env_name" {
+  type = string
+}
+
+variable "node_count" {
+  type = string
+}
+
+variable "composer_private_env" {
+  type = object({
+    enabled                = bool
+    master_ipv4_cidr_block = string
+  })
+}
+
+variable "node_config" {
+  type = object({
+    disk_size_gb      = number
+    machine_type      = string
+    enable_ip_aliases = bool
+  })
+}
+
+variable "network" {
+    type = any
+}
+variable "subnetwork" {
+    type = any
+}
+variable "service_account" {
+    type = any
+}
+
+variable "software_config" {
+  type = object({
+    image_version  = string
+    python_version = string
+    pypi_packages  = map(any)
+    env_variables  = map(any)
+    airflow_config = map(any)
+  })
+
+}
